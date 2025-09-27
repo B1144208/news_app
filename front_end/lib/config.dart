72ef11be3cc64d938dad8e23bcb54bdb125f0e03
Jiehmo <93374367+Jiehmo@users.noreply.github.com>
2025-09-27 15:54:17 +0800

    Add files via upload

--- front_end/lib/config.dart
+++ front_end/lib/config.dart
@@ -1,2 +1,6 @@
 //const String baseUrl = 'https://2d8d-59-125-186-148.ngrok-free.app/';
-const String baseUrl = 'http://localhost:3000';
+const String baseUrl = 'http://localhost:3000/api';
+
+class Config {
+  static const String apiBaseUrl = 'http://localhost:3000/api';
+}
