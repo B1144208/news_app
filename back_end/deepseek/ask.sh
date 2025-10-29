a62dec73ec6e9d569515df499c09c03921ba76d1
Bella <bella0911706330@gmail.com>
2025-10-30 04:52:29 +0800

    ask.sh

---
+++ back_end/deepseek/ask.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# 用法： ./ask.sh "QUESTION"
+
+QUESTION="$1"
+
+curl -s http://localhost:11434/api/generate \
+  -d "{
+    \"model\": \"deepseek-r1:7b\",
+    \"prompt\": \"$QUESTION\",
+    \"stream\": false
+  }" \
+| jq -r '.response' \
+| sed 's/\\u003c/</g; s/\\u003e/>/g' \
+| tr '\n' '\r' \
+| sed 's/<think>.*<\/think>//g' \
+| tr '\r' '\n'
