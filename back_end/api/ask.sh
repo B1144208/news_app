4c1e9fc3576878af98dc45574bd692d5f5767657
Bella <bella0911706330@gmail.com>
2025-10-30 08:59:43 +0800

    update:

--- back_end/api/ask.sh
+++ back_end/api/ask.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 # 用法： ./ask.sh "QUESTION"
 
-QUESTION="$1"
+QUESTION="$*"
+SAFE_PROMPT=${QUESTION//\"/\\\"}
 
-curl -s http://localhost:11434/api/generate \
-  -d '{
-    \"model\": \"deepseek-r1:7b\",
-    \"prompt\": \"$QUESTION\",
-    \"stream\": false
-  }' \
+curl -s http://localhost:11434/api/generate -d '{
+  "model": "deepseek-r1:7b",
+  \"prompt\": \"${SAFE_PROMPT}\",
+  "stream": false
+}'
 | jq -r '.response' \
 | sed 's/\\u003c/</g; s/\\u003e/>/g' \
 | tr '\n' '\r' \
