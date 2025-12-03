bf72562471e59a118331a801c620d4cf4220117b
Bella <bella0911706330@gmail.com>
2025-12-03 21:45:19 +0800

    update

--- back_end/api/routes/script.js
+++ back_end/api/routes/script.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-const { getText, getScript, reporterMake, chatMake,  callAskScript } = require('../middlewares/scriptController')
+const { getText, getScript, getQuickScipt,  callAskScript } = require('../middlewares/scriptController')
 
 // general mode
 router.get('/text', getText);
 router.get('/general', getScript);
-router.get('/quick', getScript);
+router.get('/quick', getQuickScipt);
 
 // reporter mode
 //router.get('/reporter_fast', reporterScriptFast);
