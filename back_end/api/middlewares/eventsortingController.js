f8f8d02e70ae3815e3a815f3c254b496a64df10b
LAPTOP-30FVOIT0\danie <daniel333eee@gmail.com>
2025-12-04 02:02:58 +0800

    updata

--- back_end/api/middlewares/eventsortingController.js
+++ back_end/api/middlewares/eventsortingController.js
@@ -35,7 +35,9 @@ async function searchEventsorting (req, res, next) {
         LEFT JOIN eventsorting_horizontal eh ON ed.eventsorting_id = eh.eventsorting_id
         LEFT JOIN eventsorting_vertical ev ON ed.eventsorting_id = ev.eventsorting_id
         WHERE 1
-        order by ed.total_news desc
+        GROUP BY
+            ed.eventsorting_id  -- 💡 關鍵修正：確保所有非聚合欄位都被涵蓋
+        ORDER BY ed.total_news desc;
     `;
     let params = [];
     if ( id ) {
